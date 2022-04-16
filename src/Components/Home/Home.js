81012d3c0da2f3b841dc01cdda8bdd8b5ca8933b
muntasir ahmeed <muntasirahmed5255@gmail.com>
2022-04-17 00:15:33 +0600

    first commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Naver from "../Navber/Naver";
+import './Home.css'
+const Home = () => {
+  return (
+    <section className="banner">
+      <Naver></Naver>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/Components/Navber/Naver.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Naver = () => {
+  return (
+    <div className="bg-gray-300 py-4 max-w-[1280px] mx-auto ">
+      <div className="flex justify-between px-10">
+        <div className="logo text-cyan-700 font-semibold uppercase text-2xl font-mono">
+          Family-Doc
+        </div>
+        <div className="flex items-center space-x-5 text-teal-700 font-mono text-xl font-semibold">
+          <p >Home</p>
+          <p >Services</p>
+          <p >Chekout</p>
+          <p >Login</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Naver;
